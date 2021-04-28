31af5bf3b7bdf31934b73ab7829c09b9a0872e3c
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-28 16:29:06 -0400

    Stack

---
+++ stack/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Create a stack data structure.  The stack
+// should be a class with methods 'push', 'pop', and
+// 'peek'.  Adding an element to the stack should
+// store it until it is removed.
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    pop() {
+        return this.data.pop();
+    }
+
+    push(element) {
+        this.data.push(element);
+        return this;
+    }
+
+    peek() {
+        if (this.data.length === 0) return undefined;
+        return this.data[this.data.length - 1];
+    }
+
+}
+
+module.exports = Stack;
