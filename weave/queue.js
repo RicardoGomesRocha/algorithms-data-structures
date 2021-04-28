8544aa9fc9de87de73e0fb4ebd17a5e46dd8d0aa
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-28 16:25:41 -0400

    Queues

---
+++ weave/queue.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Implement a 'peek' method in this Queue class.
+// Peek should return the last element (the next
+// one to be returned) from the queue *without*
+// removing it.
+
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+
+    add(record) {
+        this.data.unshift(record);
+    }
+
+    remove() {
+        return this.data.pop();
+    }
+
+    peek() {
+        if (this.data.length === 0) return undefined;
+        return this.data[this.data.length - 1];
+    }
+}
+
+module.exports = Queue;
